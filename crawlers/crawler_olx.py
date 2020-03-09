a48440c671035b4ecee47380bc05f354e3b9e79d
zompi <zompi2@gmail.com>
2020-03-09 21:07:27 +0100

    A lot of code added

--- crawlers/crawler_olx.py
+++ crawlers/crawler_olx.py
@@ -4,6 +4,10 @@ import soupmaker
 class CrawlerOlx(Crawler):
     pass
 
+    numofpages = -1
+    lastoffer = ""
+    cachefile = "cache/olxcache"
+
     def tofloor(self, intvalue):
         return 'floor_' + str(intvalue)
 
@@ -18,7 +22,9 @@ class CrawlerOlx(Crawler):
         }
         return switcher.get(intvalue)
 
-    def makesoup(self):
+    def makesoup(self, page):
+
+        print("Reading page nr " + str(page))
 
         url = "https://www.olx.pl/nieruchomosci/mieszkania/sprzedaz/bydgoszcz?"
  
@@ -47,20 +53,141 @@ class CrawlerOlx(Crawler):
             i+=1
 
         # sortuj od najnowszych
-        url += "search%5Border%5D=created_at%3Adesc"
+        url += "search%5Border%5D=created_at%3Adesc&"
+
+        # wybierz strone
+        url += "page=" + str(page)
+
+        print("Reading URL: " + url)
 
         return soupmaker.makesoup(url)
     
     def getlinks(self):
-        soup = self.makesoup()
 
-        table = soup.find('table', {'summary': 'Ogłoszenia'})
-        links = table.find_all('a', {'class' : 'detailsLink'})
+        uniquelinks = []
 
-        uniquelinks = set()
+        currentpage = 1
 
-        for link in links:
-            if link['href'] not in uniquelinks:
-                uniquelinks.add(link['href'])
+        while currentpage <= self.numofpages or self.numofpages == -1: 
+            soup = self.makesoup(currentpage)
+            if self.numofpages == -1:
+                self.numofpages = len(soup.findAll('span', {'class' : 'item fleft'}))
+
+            table = soup.find('table', {'summary': 'Ogłoszenia'})
+            links = table.find_all('a', {'class' : 'detailsLink'})
+
+            for link in links:
+                href = link['href']
+                if href == self.lastoffer:
+                    return uniquelinks
+                if href not in uniquelinks:
+                    uniquelinks.append(href)
+
+            currentpage = currentpage + 1
         
         return uniquelinks
+    
+    def checkforfordon(self, soup):
+        
+        badwords = ["Fordon", "FORDON", "fordon", "Bajka", "BAJKA", "bajka"]
+        
+        map = soup.find('a', {'href' : '#map'}) #otodom map
+        if map:
+            for content in map.contents:
+                if "Fordon" in content:
+                    print("Fordon Found")
+                    return True
+        
+        title = soup.find('div', {'class' : 'offer-titlebox'})
+        if title:
+            for badword in badwords:
+                if badword in title.text:
+                    print("Fordon Found")
+                    return True
+
+        desc = soup.find('section', {'class' : 'section-description'}) #otodom desc
+        if desc:
+            ps = desc.findAll('p')
+            for p in ps:
+                for badword in badwords:
+                    if badword in p.text:
+                        print("Fordon Found")
+                        return True
+                        
+        desc2 = soup.find('div', {'id' : 'textContent'}) #olx desc
+        if desc2:
+            for badword in badwords:
+                if badword in desc2.text:
+                    print("Fordon Found")
+                    return True
+
+
+        return False      
+
+    def checkforfloor(self, soup):
+        overview = soup.find('section', {'class' : 'section-overview'})
+        if overview:
+            points = overview.findAll('li')
+            floor = -1
+            allfloors = -1
+            for point in points:
+                if "Piętro: 4" in point.text:
+                    floor = 4
+                if "Liczba pięter: 4" in point.text:
+                    allfloors = 4
+            if floor == 4 and allfloors == 4:
+                print("Last four floor Found")
+                return True
+        return False
+
+    def filteroffers(self, offers):
+        filteredoffers = []
+
+        idx = 1
+        for offer in offers:
+            print("checking offer %i / %i" % (idx, len(offers)))
+            idx = idx+1
+            soup = soupmaker.makesoup(offer)
+
+            # check for Fordon
+            if self.checkforfordon(soup) :
+                continue     
+
+            # check for last floor (4th)
+            if self.checkforfloor(soup) :
+                continue
+
+            filteredoffers.append(offer)         
+
+        return filteredoffers
+
+    def getoffers(self, onlynew):
+
+        print("Getting offers fro OLX")
+
+        cachedoffers = []
+
+        if onlynew:
+            try:
+                file = open(self.cachefile, "r")
+                cachedoffers = file.readlines()
+                if len(cachedoffers) > 0 :
+                    print("Last found offer was: " + cachedoffers[0])
+                    self.lastoffer = cachedoffers[0][:-1]
+                    file.close()
+            except:
+                print("There is no Last offer found")
+        else:
+            print("Getting all offers")
+        
+        alloffers = self.getlinks()
+
+        file = open(self.cachefile, "w")
+        
+        for offer in alloffers:
+            file.write('%s\n' % offer)
+        for offer in cachedoffers:
+             file.write(offer)
+        file.close()
+
+        return self.filteroffers(alloffers)
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import os
 from crawlers.crawler_olx import CrawlerOlx
 
 print("Hello Home Scanner!")
+
+if not os.path.exists("cache"):
+    print("Creating cache directory")
+    os.makedirs("cache")
 
 crawler = CrawlerOlx()
 
@@ -10,10 +15,11 @@ crawler.fromsize = 56
 crawler.floors = [1,2,3,4,5,6]
 crawler.rooms = [3,4]
 
-links = crawler.getlinks()
+links = crawler.getoffers(True)
 
-file = open("testfile.html", "w")
-file.write(str(links))
+file = open("result.html", "w")
+for link in links:
+    file.write('<p><a href="%s">%s</a></p>\n' % (link, link))
 file.close()
 
 print("Scan complete")
